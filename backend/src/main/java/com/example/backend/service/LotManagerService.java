b40d7980ffa21e68457767abe1ea433e4049432a
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-12-27 01:02:17 +0200

    feat: functions to get Ids from context of springboot

--- backend/src/main/java/com/example/backend/service/LotManagerService.java
+++ backend/src/main/java/com/example/backend/service/LotManagerService.java
@@ -1,9 +1,12 @@
 package com.example.backend.service;
 
 import com.example.backend.DTOs.SuccessLoginDTO;
+import com.example.backend.model.CustomUserDetails;
 import com.example.backend.model.LotManager;
 import com.example.backend.repository.LotManagerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -46,5 +49,13 @@ public class LotManagerService {
         }
         String jwt = jwtService.generateToken(lotManager.getId(), lotManager.getRole());
         return new SuccessLoginDTO(lotManager.getId(), lotManager.getUsername(), lotManager.getRole(), jwt);
+    }
+
+    public int getManagerId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails)) {
+            throw new RuntimeException("User is not authenticated or invalid principal");
+        }
+        return ((CustomUserDetails)authentication.getPrincipal()).getUserId();
     }
 }
